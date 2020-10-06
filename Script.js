617b5dea1c7a30c14353a06ba421c4a398473f94
Jebsito <a325060@uach.mx>
2020-10-05 19:27:15 -0600

    Funcionamiento a medias

--- Script.js
+++ Script.js
@@ -1,8 +1,47 @@
-function Activacion(MesaX, IniX, FinX){
+function Activacion(MesaX, ini, fin, tot){
     mesa = document.getElementById(MesaX);
     if(mesa.src.match('TableOff.png')){
         mesa.src = "TableOn.png"
+        tiempo(ini);
+        crono(fin, MesaX, tot);
+    
     }else{
         mesa.src = "TableOff.png"
     }
 }
+
+
+const tiempo = (ini) =>{
+    time = new Date();
+    horas = time.getHours()
+    minutos = time.getMinutes()
+    segundos = time.getSeconds()
+    tiempotot = horas + " ' " + minutos + " '' " + segundos + " ''' ";
+    document.getElementById(ini).innerHTML = tiempotot;
+}
+
+function crono(fin, MesaX, tot){
+    var myVar = setInterval(myTimer, 1000);
+    var seg  = 0;
+    mesa = document.getElementById(MesaX);
+    function myTimer() {
+      var d = new Date();
+      document.getElementById(fin).innerHTML = seg++;
+      if(mesa.src.match('TableOff.png')){
+        clearInterval(myVar);
+        document.getElementById(totis).innerHeight = "perro";
+        total(tot, seg);
+      }
+    }
+}
+
+function total(tot, seg){
+    totis = document.getElementById(tot);
+    document.getElementById(totis).innerHeight = "perro";
+}
+
+
+
+
+
+
